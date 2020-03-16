62241347c08f5979d9c704d93290229f364bb122
Noordeen Malango <noordinmalango@gmail.com>
2020-03-16 16:05:11 +0200

    seaborn code

--- data_visualization.py
+++ data_visualization.py
@@ -5,5 +5,25 @@ import  seaborn as sns
 
 df = pd.read_csv('Pokemon.csv',index_col=0,encoding='ISO-8859-1')
 
-sns.lmplot(x='Attack',y='Defense', data=df)
+sns.lmplot(x='Attack',y='Defense', data=df, fit_reg=False, hue='Stage')
+
+#customizing the axes with matplotlib
+plt.ylim(0,None)
+plt.xlim(0,None)
+
+# Pre-format DataFrame
+stats_df = df.drop(['Total', 'Stage', 'Legendary'], axis=1)
+# New boxplot using stats_df
+#sns.boxplot(data=stats_df)
+
+#setting theme
+sns.set_style('whitegrid')
+
+#visualise the distribution of attach by pokemons type using violinplot
+#sns.violinplot(x='Type 1', y='Attack', data=df)
+# Swarm plot with Pokemon color palette
+#sns.swarmplot(x='Type 1', y='Attack', data=df)
+
+# Distribution Plot (a.k.a. Histogram)
+sns.distplot(df.Attack)
 plt.show()
